bdeed4b44c5145ddc551ef96ab0f57706533202a
dyakonov <dyakonov@protei-lab.ru>
2022-05-17 17:17:18 +0300

    finish starting task

--- cows.cpp
+++ cows.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 // mod(p) = X, x принадлежит [1,p), [x^1-1, x^2-1, ... x^(p-2)-1]%p != 0, (x^(p-1) - 1) % p = 0
 
 //input: p [2<=p<2000]
@@ -21,7 +22,10 @@ bool is_simple(int value)
     }
     return true;
 }
-
+bool is_devisible_on_input(int powered_value, int user_input)
+{
+    return ((powered_value - 1) % user_input) == 0;
+}
 int main(int argc, char** argv)
 {
     //handle correct input
@@ -45,12 +49,37 @@ int main(int argc, char** argv)
             show_help(base_string + " is not a simple number");
             return -1;
         }
-        
-        std::cout<<"hello: "<<inputted_number_p<<std::endl;
-
+        std::cout<<"you have inputted: "<<inputted_number_p<<std::endl;
     }
     else {
-        show_help();
+        show_help("incorrect arguments!");
+        return -1;
     }
+
+    //main logic
+    int amount_of_primitive_roots = 0;
+    for(int value_challenger = 1; value_challenger < inputted_number_p; value_challenger++) {
+        int first_powered = pow(value_challenger, inputted_number_p - 1);
+        if( not is_devisible_on_input(first_powered, inputted_number_p)) {
+            continue;
+        }
+        bool is_valid = true;
+        for(int power = 1; power <= inputted_number_p - 2; power++) {
+            if(is_devisible_on_input(pow(value_challenger, power), inputted_number_p)) {
+                is_valid = false;
+                break;
+            }
+        }
+        if (is_valid)
+        {
+            if(amount_of_primitive_roots == 0) {
+                std::cout <<"primitive roots are: ";
+            }
+            std::cout <<"["<<value_challenger <<"] ";
+            amount_of_primitive_roots++;
+        }
+    }
+    std::cout<<"\nTotal amount of primitive roots is: "<<amount_of_primitive_roots<<std::endl;
+
     return 0;
 }
