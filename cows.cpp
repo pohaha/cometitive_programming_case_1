f4f21267e378df12753778f06f8a6cebcf9916b1
dyakonov <dyakonov@protei-lab.ru>
2022-05-17 16:26:07 +0300

    add initial files, create working cmake project

---
+++ cows.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+// mod(p) = X, x принадлежит [1,p), [x^1-1, x^2-1, ... x^(p-2)-1]%p != 0, (x^(p-1) - 1) % p = 0
+int main(int argc, char** argv)
+{
+    std::cout<<"Hello world!"<<std::endl;
+    return 0;
+}
